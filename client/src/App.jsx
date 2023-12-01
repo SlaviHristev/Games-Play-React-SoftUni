20fa5f4d2f68bfa58ed61fe1b6148ec587f1adc0
SlaviHristev <Slavihristev97@gmail.com>
2023-12-01 19:29:00 +0200

    Started implementing Routes in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
-import Header from "./components/Header/Header"
+import {Routes,Route} from 'react-router-dom'
 import '../styles/style.css'
 import Home from "./components/Home/Home"
+import Header from "./components/Header/Header"
+import Create from './components/Create/Create'
+import Catalog from './components/Catalog/Catalog'
+import Login from './components/Login/Login'
+import Register from './components/Register/Register'
 
 
 function App() {
@@ -8,9 +13,14 @@ function App() {
 
   return (
    <div id="box">
-        <Header/>
-        <Home/>
-
+    <Header/>
+    <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='create' element={<Create/>}/>
+       <Route path='catalog' element={<Catalog/>}/>
+       <Route path='login' element={<Login/>}/>
+       <Route path='register' element={<Register/>}/>
+    </Routes>
    </div>
   )
 }
